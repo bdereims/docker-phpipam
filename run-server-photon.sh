8f235ea11b493ee0eabb4ee62a24ef42331776a1
Brice Dereims <bdereims@gmail.com>
2015-09-11 14:05:25 +0200

    add photon script and README.md

--- run-server-photon.sh
+++ run-server-photon.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-VOLUME=/data/lv-raid5/Docker/Volumes/mysql-phpmyadmin
+VOLUME=/root/mysql
+
+mkdir -p $VOLUME
 
 docker run -d -p 3306:3306 -p 8081:80 -v ${VOLUME}:/var/lib/mysql bdereims/mysql-phpmyadmin
